fa20ea4225ddc40ba3d517f71e44ed42b9f4764b
unknown <aonguyen113@gmail.com>
2020-02-14 14:16:30 +0700

    test api key

--- src/api/services/weather.service.js
+++ src/api/services/weather.service.js
@@ -8,26 +8,35 @@ module.exports = {
 			let result;
 			const darksky_api_key = process.env.DARK_SKY_KEY;
 
-			// const options = {
-			// 	provider: "openstreetmap",
-			// 	httpAdapter: 'https',
-			// 	formatter: null
-			// }
-			// const geocoder = nodeGeocoder(options);
-			// const rs = await geocoder.geocode(query);
+			const options = {
+				provider: "google",
+				httpAdapter: 'https',
+				formatter: null,
+				apiKey: process.env.GOOGLE_MAP_API_KEY
+			}
+			console.log(query)
+
+			const geocoder = nodeGeocoder(options);
+			geocoder.reverse({lat: 14.058324, lon: 108.277199}).then(res => {
+				result = res;
+			})
+			return {
+				code: httpStatus.OK,
+				payload: result
+			}
 			// const { latitude, longitude } = rs[0];
 
 			// console.log(latitude, longitude)
 
-			await fetch(`https://api.darksky.net/forecast/${darksky_api_key}/14.058324,108.277199`)
-				.then(res => res.json())
-				.then(json =>  {
-					result = json;
-				})
-			return {
-				code: httpStatus.OK,
-				payload: result
-			};
+			// await fetch(`https://api.darksky.net/forecast/${darksky_api_key}/14.058324,108.277199`)
+			// 	.then(res => res.json())
+			// 	.then(json =>  {
+			// 		result = json;
+			// 	})
+			// return {
+			// 	code: httpStatus.OK,
+			// 	payload: result
+			// };
 		} catch (e) {
 			return {
 				code: httpStatus.INTERNAL_SERVER_ERROR
